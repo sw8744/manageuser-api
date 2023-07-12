64299237c557c4b76bb5008824a186fa65b0a8ff
thislife_won <sw8744@naver.com>
2023-07-12 18:36:06 +0900

    commit

---
+++ server.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql2');
+var session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: 'test'
+});
+
+
+var options = {
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: 'sessions'
+};
+
+var sessionStore = new MySQLStore(options);
+
+connection.connect(function (err) {
+    if (err) throw err;
+    else {
+        console.log('Successfully Connected');
+    }
+});
+
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    HttpOnly: true,
+    store: sessionStore
+}));
+
+
+app.get('/register', function (req, res) {
+    const q = req.query;
+    const gisu = q.gisu;
+    const stunum = q.stunum;
+    const id = q.id;
+    const privilege = q.privilege;
+    const password = q.password;
+    var bool_key = false;
+    var bool_name = false;
+    if(stunum != null && stunum.length == 4 && id != null && privilege != null && password != null && password != null) {
+        connection.query('SELECT `key` FROM users WHERE `key`=' + gisu + stunum, function (err, result) {
+            if (err) throw err;
+            if(result.length > 0) {
+                bool_key = true;
+                res.json('Same_Key_Exists');
+            }
+        });
+        connection.query('SELECT name FROM users WHERE name=\'' + id + '\'', function(err, result) {
+            if (err) throw err;
+            if(result.length > 0) { 
+                bool_name = true;
+                res.json('Same_ID_Exists');
+            }
+        })
+        if(bool_name == false && bool_key == false) {
+            connection.query('INSERT INTO users VALUES (' + gisu + stunum + ', \'' + id + '\', ' + privilege + ', ' + password + ');', function (err, result) {
+                if (err) throw err;
+                res.json('Successfully_Registered');
+            });
+        }
+    }
+    else {
+        res.json('Error');
+    }
+});
+
+app.get('/login', function(req, res) {
+    const q = req.query;
+    const id = q.id;
+    const pw = q.password;
+    connection.query('SELECT * FROM users WHERE name=\'' + id + '\'', function(err, result) {
+        if (err) throw err;
+        if(result.length != 0) {
+            const pw_db = result[0].password;
+            if(pw_db === pw) {
+                req.session.id = result[0].name;
+                req.session.stunum = result[0].key;
+                req.session.privilege = result[0].privilege;
+                req.session.isLogined = true;
+            }
+            else {
+                res.json('Incorrect_Password');
+            }
+        }
+        else {
+            res.json('ID_Not_Exists');
+        }
+    });
+});
+
+app.listen(3000);
